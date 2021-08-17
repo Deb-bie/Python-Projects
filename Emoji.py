bf8d4a11d0636b51d5c751a00973375053342382
Debbie <asamoahjaeldeborah@gmail.com>
2021-08-17 14:33:17 +0000

    Initial commit

---
+++ Emoji.py
@@ -0,0 +1,19 @@
+print("\U0001f600", sep="")
+
+print("\U0001f617")
+
+print("\U0001f602")
+
+print("\U0001f603")
+
+print("\U0001f604")
+
+print("\U0001f605")
+
+print("\U0001f620")
+
+
+print("\U0001f630")
+
+
+
